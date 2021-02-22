9cfbd426bc45006a15daf82d7f0efaaa871eae21
Sam Purkis <sam.purkis@hotmail.co.uk>
2021-02-22 23:10:23 +0000

    added implentation to maintain pandas column ordering and tests for each case

--- fast_json_normalize/__init__.py
+++ fast_json_normalize/__init__.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 
-def fast_json_normalize(json_object: list or dict, separator: str = ".", to_pandas: bool = False):
-    # main recursive function, maintains object types
+def fast_json_normalize(json_object: list or dict, separator: str = ".", to_pandas: bool = True,
+                        order_to_pandas: bool = True):
+    # main recursive function, maintains object types, not ordering
     def _normalise_json(object_: list or dict, key_string_: str = "", new_dict_: dict = None, separator_: str = "."):
         if isinstance(object_, dict):
             for key, value in object_.items():
@@ -20,18 +21,65 @@ def fast_json_normalize(json_object: list or dict, separator: str = ".", to_pand
             new_dict_[key_string_] = object_
         return new_dict_
 
+    # depth aware recursive function, maintains object types and pandas column ordering
+    def _normalise_json_ordered(object_: list or dict, key_string_: str = "", nested_dict_: dict = None,
+                                top_dict_: dict = None,
+                                separator_: str = ".", depth: int = 0):
+        if isinstance(object_, dict):
+            for key, value in object_.items():
+                new_key = f"{key_string_}{separator_}{key}"
+                _normalise_json_ordered(object_=value,
+                                        key_string_=new_key if new_key[len(separator_) - 1] != separator_
+                                        else new_key[len(separator_):],
+                                        # to avoid adding the separator to the start of every key
+                                        nested_dict_=nested_dict_,
+                                        top_dict_=top_dict_,
+                                        separator_=separator_,
+                                        depth=depth + 1)
+        elif isinstance(object_, list):
+            if depth == 1:
+                top_dict_[key_string_] = object_
+            else:
+                nested_dict_[key_string_] = object_
+        elif isinstance(object_, str) or isinstance(object_, int) or isinstance(object_, float):
+            if depth == 1:
+                top_dict_[key_string_] = object_
+            else:
+                nested_dict_[key_string_] = object_
+        else:
+            if depth == 1:
+                top_dict_[key_string_] = object_
+            else:
+                nested_dict_[key_string_] = object_
+        return top_dict_, nested_dict_
+
     # expect a dictionary, as most jsons are. However, lists are perfectly valid
     if isinstance(json_object, dict):
-        normalised_json_object = _normalise_json(object_=json_object,
-                                                 separator_=separator,
-                                                 new_dict_={})
+        if not order_to_pandas:
+            normalised_json_object = _normalise_json(object_=json_object,
+                                                     separator_=separator,
+                                                     new_dict_={})
+        else:
+            top_dict, nested_dict = _normalise_json_ordered(object_=json_object,
+                                                            separator_=separator,
+                                                            nested_dict_={},
+                                                            top_dict_={},
+                                                            depth=0)
+            normalised_json_object = {**top_dict, **nested_dict}
         if to_pandas:
-            return pd.DataFrame(data=[normalised_json_object.values()],
-                                columns=list(normalised_json_object.keys()))
+            df = pd.DataFrame(data=[normalised_json_object.values()],
+                              columns=list(normalised_json_object.keys()))
+            return df
     elif isinstance(json_object, list):
-        normalised_json_object = [_normalise_json(row,
-                                                  separator_=separator,
-                                                  new_dict_={}) for row in json_object]
+        if not order_to_pandas:
+            normalised_json_object = [_normalise_json(row,
+                                                      separator_=separator,
+                                                      new_dict_={}) for row in json_object]
+        else:
+            normalised_json_object = [fast_json_normalize(row,
+                                                          separator=separator,
+                                                          order_to_pandas=order_to_pandas,
+                                                          to_pandas=False) for row in json_object]
         if to_pandas:
             return pd.DataFrame(normalised_json_object)
     else:
--- tests/test_main.py
+++ tests/test_main.py
@@ -8,7 +8,7 @@ from fast_json_normalize import fast_json_normalize
 # example json data
 data = {"hello": ["thisisatest", 999898, datetime.date.today()],
         "nest1": {"nest2": {"nest3": "nest3_value", "nest3_int": 3445}},
-        "nested_list": ["blah", 32423, 546456.876, 92030234],
+        "nest1_list": {"nest2": ["blah", 32423, 546456.876, 92030234]},
         "hello2": "string"}
 hundred_rows = [data for i in range(100)]
 ten_thousand_rows = [data for i in range(10000)]
@@ -16,18 +16,17 @@ hundred_thousand_rows = [data for i in range(100000)]
 million_rows = [data for i in range(1000000)]
 
 
-def timeit(func, arguments):
-    start = time()
-    func(arguments)
-    return time() - start
-
-
 def test_pandas_dict_equality():
     pandas_json_normalize_df = pd.json_normalize(data)
     fast_json_normalize_df = fast_json_normalize(data, to_pandas=True)
-    p = pandas_json_normalize_df.to_records()
-    f = fast_json_normalize_df.to_records()
     assert set(pandas_json_normalize_df.columns) == set(fast_json_normalize_df.columns)
+    assert all(pandas_json_normalize_df.to_records() == fast_json_normalize_df.to_records())
+
+
+def test_pandas_dict_equality_ordered():
+    pandas_json_normalize_df = pd.json_normalize(data)
+    fast_json_normalize_df = fast_json_normalize(data, to_pandas=True, order_to_pandas=True)
+    assert all(pandas_json_normalize_df.columns == fast_json_normalize_df.columns)
     assert all(pandas_json_normalize_df.to_records() == fast_json_normalize_df.to_records())
 
 
@@ -38,33 +37,66 @@ def test_pandas_list_equality():
     assert all(pandas_json_normalize_df.to_records() == fast_json_normalize_df.to_records())
 
 
+def test_pandas_list_equality_ordered():
+    pandas_json_normalize_df = pd.json_normalize(hundred_rows)
+    fast_json_normalize_df = fast_json_normalize(hundred_rows, to_pandas=True, order_to_pandas=True)
+    assert all(pandas_json_normalize_df.columns == fast_json_normalize_df.columns)
+    assert all(pandas_json_normalize_df.to_records() == fast_json_normalize_df.to_records())
+
+
 def test_speed_hundred_rows():
-    pandas_json_normalize_time_taken = timeit(pd.json_normalize, hundred_rows)
+    s = time()
+    pd.json_normalize(hundred_rows)
+    pandas_json_normalize_time_taken = time() - s
     print(f"\npandas time taken for a 100 rows: {pandas_json_normalize_time_taken}")
-    fast_json_normalize_time_taken = timeit(fast_json_normalize, hundred_rows)
+    s = time()
+    fast_json_normalize(hundred_rows, to_pandas=True, order_to_pandas=True)
+    fast_json_normalize_time_taken = time() - s
     print(f"fast implementation time taken for a 100 rows: {fast_json_normalize_time_taken}")
     assert fast_json_normalize_time_taken < pandas_json_normalize_time_taken
 
 
 def test_speed_ten_thousand_rows():
-    pandas_json_normalize_time_taken = timeit(pd.json_normalize, ten_thousand_rows)
-    print(f"\npandas time taken for 10,000 rows: {pandas_json_normalize_time_taken}")
-    fast_json_normalize_time_taken = timeit(fast_json_normalize, ten_thousand_rows)
-    print(f"fast implementation time taken for 10,000 rows: {fast_json_normalize_time_taken}")
+    s = time()
+    pd.json_normalize(ten_thousand_rows)
+    pandas_json_normalize_time_taken = time() - s
+    print(f"\npandas time taken for a 10,000 rows: {pandas_json_normalize_time_taken}")
+    s = time()
+    fast_json_normalize(ten_thousand_rows, to_pandas=True, order_to_pandas=True)
+    fast_json_normalize_time_taken = time() - s
+    print(f"fast implementation time taken for a 10,000 rows: {fast_json_normalize_time_taken}")
     assert fast_json_normalize_time_taken < pandas_json_normalize_time_taken
 
 
 def test_speed_hundred_thousand_rows():
-    pandas_json_normalize_time_taken = timeit(pd.json_normalize, hundred_thousand_rows)
-    print(f"\npandas time taken for 100 for a 100,000 rows: {pandas_json_normalize_time_taken}")
-    fast_json_normalize_time_taken = timeit(fast_json_normalize, hundred_thousand_rows)
+    s = time()
+    pd.json_normalize(hundred_thousand_rows)
+    pandas_json_normalize_time_taken = time() - s
+    print(f"\npandas time taken for a 100,000 rows: {pandas_json_normalize_time_taken}")
+    s = time()
+    fast_json_normalize(hundred_thousand_rows, to_pandas=True, order_to_pandas=False)
+    fast_json_normalize_time_taken = time() - s
     print(f"fast implementation time taken for a 100,000 rows: {fast_json_normalize_time_taken}")
     assert fast_json_normalize_time_taken < pandas_json_normalize_time_taken
 
 
-def test_speed_million_rows():
-    pandas_json_normalize_time_taken = timeit(pd.json_normalize, million_rows)
-    print(f"\npandas time taken for 100 for a million rows: {pandas_json_normalize_time_taken}")
-    fast_json_normalize_time_taken = timeit(fast_json_normalize, million_rows)
-    print(f"fast implementation time taken for a million rows: {fast_json_normalize_time_taken}")
+def test_speed_hundred_thousand_rows_ordered():
+    s = time()
+    pd.json_normalize(hundred_thousand_rows)
+    pandas_json_normalize_time_taken = time() - s
+    print(f"\npandas time taken for a 100,000 rows: {pandas_json_normalize_time_taken}")
+    s = time()
+    fast_json_normalize(hundred_thousand_rows, to_pandas=True, order_to_pandas=True)
+    fast_json_normalize_time_taken = time() - s
+    print(
+        f"fast implementation (maintaining pandas order) time taken for a 100,000 rows: {fast_json_normalize_time_taken}")
     assert fast_json_normalize_time_taken < pandas_json_normalize_time_taken
+
+# def test_speed_million_rows():
+#     s = time()
+#     pandas_json_normalize_time_taken = pd.json_normalize(million_rows)
+#     print(f"\npandas time taken for a 100 rows: {time() - s}")
+#     s = time()
+#     fast_json_normalize_time_taken = fast_json_normalize(million_rows, to_pandas=True, order_to_pandas=True)
+#     print(f"fast implementation time taken for a 100 rows: {time() - s}")
+#     assert fast_json_normalize_time_taken < pandas_json_normalize_time_taken
