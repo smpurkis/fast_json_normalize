2190a3d821c8c2979a40112c91c8abb29c9a1079
Sam Purkis <sam.purkis@hotmail.co.uk>
2021-03-15 20:33:07 +0000

    [setup] regex the folder containing cythonized.pyx

--- setup.py
+++ setup.py
@@ -8,18 +8,17 @@ with open("README.md", 'r') as f:
 
 setup(
     name="fast_json_normalize",
-    version="0.0.8",
+    version="0.0.7a",
     packages=find_packages(),
     author="Sam Purkis",
     description="A classification library using a novel audio-inspired algorithm.",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/smpurkis/fast_json_normalize",
-    ext_modules=cythonize(["fast_json_normalize/cythonized.pyx"]),
+    ext_modules=cythonize(["**/cythonized.pyx"]),
     install_requires=[
         'pytest',
         'cython',
         'pandas',
-        'setuptools'
     ],
 )
